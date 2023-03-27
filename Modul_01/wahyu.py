5c8c453383a8b88041398da2f2923797a965a4fd
wahyu <wahyubudik9@student.uns.ac.id>
2023-03-27 13:16:01 +0700

    Exercise-02&03

---
+++ Modul_01/wahyu.py
@@ -0,0 +1,14 @@
+nama_depan = "Wahyu"
+nama_belakang = "Budi"
+kelas = "D"
+nim = "128"
+
+angka = 10
+desimal = 0,5
+boolean = True
+kelas_d = ["Wahyu", "Kelas D", "dll"]
+data = {"Nama": "Wahyu", "Kelas": "Kls D","Nim": 128}
+
+print(nama_depan)
+print("nama saya adalah Wahyu")
+print(f"nama saya adalah {nama_depan} {nama_belakang} dari kelas {kelas} dengan Nim {nim}")
